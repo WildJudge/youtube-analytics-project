29e3dc678ae9417c1f306f07c792af77f4a05178
WildJudge <wildjudge90@gmail.com>
2023-09-06 18:19:22 +0300

    Дописан код для класса Channel в channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import os
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +13,27 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        # Получение данных о канале
+        channel_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel_data = channel_info.get('items', [])[0]
+
+        if channel_data:
+            # Извлечение нужных данных
+            channel_title = channel_data['snippet']['title']
+            description = channel_data['snippet']['description']
+            view_count = channel_data['statistics']['viewCount']
+            subscriber_count = channel_data['statistics']['subscriberCount']
+            video_count = channel_data['statistics']['videoCount']
+
+            # Вывод информации
+            print(f"Название канала: {channel_title}")
+            print(f"Описание: {description}")
+            print(f"Количество просмотров: {view_count}")
+            print(f"Количество подписчиков: {subscriber_count}")
+            print(f"Количество видео: {video_count}")
+        else:
+            print("Канал с указанным ID не найден.")
