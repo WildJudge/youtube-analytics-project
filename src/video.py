0f2007836f4eb26e39f52e714728fece4a83c2bd
WildJudge <wildjudge90@gmail.com>
2023-09-24 15:38:54 +0300

    Создан playlist.py. В video.py в класс PLVideo добавлены методы. В main.py исправлена строка assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM" на assert pl.show_best_video() == "https://www.youtube.com/watch?v=cUGyMzWQcGM"

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+import datetime
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
@@ -37,3 +38,42 @@ class PLVideo(Video):
         """Инициализация видео с указанием ID видео и ID плейлиста"""
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
+    def _init_from_api(self) -> None:
+        video_info = self.service.videos().list(id=self.video_id, part='snippet,statistics,contentDetails').execute()
+        video_info = video_info['items'][0]
+
+        self.id = video_info['id']
+        self.title = video_info['snippet']['title']
+        self.url = f'https://www.youtube.com/watch?v={self.id}'
+        self.view_count = video_info['statistics']['viewCount']
+        self.like_count = video_info['statistics']['likeCount']
+
+        # Получаем длительность видео в секундах
+        content_details = video_info.get('contentDetails', {})
+        duration_iso = content_details.get('duration', '')
+        self.duration = self.parse_duration(duration_iso)
+
+    @staticmethod
+    def parse_duration(duration_iso: str) -> datetime.timedelta:
+        """Преобразует строку длительности из формата ISO 8601 в объект timedelta"""
+        if not duration_iso:
+            return datetime.timedelta()
+
+        parts = duration_iso.split('T')
+        time_parts = parts[-1].split('M')
+        hours, minutes, seconds = 0, 0, 0
+
+        if 'H' in time_parts[0]:
+            hours = int(time_parts[0].split('H')[0])
+            time_parts[0] = time_parts[0].split('H')[1]
+
+        if 'S' in time_parts[0]:
+            seconds = int(time_parts[0].split('S')[0])
+
+        if len(time_parts) > 1:
+            minutes = int(time_parts[0])
+            if 'S' in time_parts[1]:
+                seconds = int(time_parts[1].rstrip('S'))
+
+        return datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
